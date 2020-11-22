bb79a0cd4f18773435fb848be4827bd68039e5e8
Metogbe Rufin Hounkpe <rhounkpe@gmail.com>
2020-11-22 16:34:32 +0100

    Business Logic + Unit Test

---
+++ src/test/java/be/digitcom/calculator/CalculatorShould.java
@@ -0,0 +1,14 @@
+package be.digitcom.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorShould {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void sum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
