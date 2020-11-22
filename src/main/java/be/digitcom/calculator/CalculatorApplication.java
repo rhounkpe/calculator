322e0853ee2943688bd1aea5c38bfbf65d9da375
Metogbe Rufin Hounkpe <rhounkpe@gmail.com>
2020-11-22 15:39:02 +0100

    Initial state

---
+++ src/main/java/be/digitcom/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
